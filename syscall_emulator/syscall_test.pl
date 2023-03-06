a80d5929f49b457b64efe3cf5cd0c111477b3d09
Andrew Hewus Fresh <andrew@afresh1.com>
2023-03-05 18:39:06 -0800

    Add some tests for mmap/munmap
    Although I have no idea how to actually get the value on the other
    end of the pointer that gets returned, so I don't know how useful
    it is.
    
    I don't think I get the proper value anyway since the munmap fails
    when I pass it that value.

--- syscall_emulator/syscall_test.pl
+++ syscall_emulator/syscall_test.pl
@@ -5,6 +5,12 @@ use Test::More;
 use File::Temp;
 use POSIX qw< S_IRUSR S_IWUSR S_IRGRP S_IROTH O_CREAT O_WRONLY O_RDONLY >;
 
+use constant {
+    PROT_READ   => 0x01,
+    MAP_PRIVATE => 0x0002,
+    MAP_FAILED  => -1,
+};
+
 my $dir = File::Temp->newdir("syscall_emulator-XXXXXXXXX");
 {
 	system("h2ph", '-d', $dir,
@@ -18,12 +24,13 @@ my $file = "$dir/$filename";
 my $fd;
 my $out = "Hello World\n";
 my $in = "\0" x 32;
+my $in_p;
 my $sb = "\0\0\0\0";
 my $st_mode;
 
 my $perms = S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH;
 
-plan tests => 9;
+plan tests => 14;
 
 ok(!
     (($fd = syscall(SYS_open(), $file, O_CREAT|O_WRONLY, $perms)) < 0),
@@ -60,13 +67,39 @@ ok(!
     (syscall(SYS_read(), $fd, $in, length $in) <= 0),
     "read from $filename"
 );
-ok(!
-    (syscall(SYS_close(), $fd) != 0),
-    "closed $filename"
-);
 
 $in = unpack 'Z*', $in;
 
 ok( length($in) == length($out) && ($in eq $out),
     "Read written content from $filename"
+);
+
+ok(!
+    (syscall(SYS_lseek(), $fd, 0, SEEK_SET) < 0),
+    "lseek on fd"
+);
+
+
+ok(!
+    (($in_p = syscall(SYS_mmap(), undef, length($out), PROT_READ, MAP_PRIVATE,
+        $fd, 0)) == MAP_FAILED),
+    "mmap fd"
+);
+
+ok $in_p =~ /^-?\d+$/, "The mmapped value ($in_p) looks like an integer";
+
+SKIP: { skip "No idea how to get dereference the pointer", 2;
+ok( length($in_p) == length($out) && ($in_p eq $out),
+    "Read written content from $filename"
+);
+
+ok(!
+    (syscall(SYS_munmap(), $in_p, length($out)) != 0),
+    "munmap fd"
+);
+}
+
+ok(!
+    (syscall(SYS_close(), $fd) != 0),
+    "closed $filename"
 );
--- syscall_emulator/syscall_user.c
+++ syscall_emulator/syscall_user.c
@@ -1,12 +1,15 @@
 #include <sys/syscall.h>
 #include "syscall_emulator.h"
 
+#include <sys/mman.h>
+#include <sys/stat.h>
+
+#include <err.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/stat.h>
-#include <err.h>
-#include <fcntl.h>
+#include <unistd.h>
 
 int
 main(void) {
@@ -14,12 +17,13 @@ main(void) {
 	char file[] = "./test.out";
 	char out[] = "Hello World\n";
 	char in[32];
+	char *in_p;
 	mode_t perms = S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH;
 	struct stat *sb;
 
 	sb = malloc(sizeof(struct stat));
 
-	printf("%s\n", "1..9");
+	printf("%s\n", "1..13");
 
 	if ((fd = syscall_emulator(SYS_open, file, O_CREAT|O_WRONLY, perms)) < 0)
 		err(1, "Failed to open test.out for write/create");
@@ -53,16 +57,33 @@ main(void) {
 		err(1, "Failed to read");
 	printf("ok 7 read %lu bytes from %s\n", strlen(in), file);
 
-	if (syscall_emulator(SYS_close, fd) != 0)
-		err(1, "Failed to close");
-	printf("ok 8 closed %s\n", file);
-
 	if (strlen(in) == strlen(out)
 	 && strncmp(in, out, sizeof(out)) == 0)
-		printf("ok 9 Read written content from %s\n", file);
+		printf("ok 8 Read written content from %s\n", file);
 	else
-		printf("not ok 9 Read written content from %s\n", file);
+		printf("not ok 8 Read written content from %s\n", file);
 
+	if (syscall_emulator(SYS_lseek, fd, 0, SEEK_SET) < 0)
+		err(1, "Failed to lseek");
+	printf("ok 9 lseek on fd\n");
+
+	if ((in_p = (void *)syscall_emulator(SYS_mmap,
+	    NULL, sizeof(out), PROT_READ, MAP_PRIVATE,
+	    fd, 0)) == MAP_FAILED)
+		err(1, "mmap failed");
+	printf("ok 10 mmap fd\n");
+
+	if (strncmp(in_p, out, sizeof(out)) == 0)
+		printf("ok 11 Read written content from %s via mmap\n", file);
+	else
+		printf("not ok 11 Read written content from %s via mmap\n", file);
+	if (syscall_emulator(SYS_munmap, in_p, sizeof(out)) != 0)
+	    err(1, "munmap failed");
+	printf("ok 12 munmap fd\n");
+
+	if (syscall_emulator(SYS_close, fd) != 0)
+		err(1, "Failed to close");
+	printf("ok 13 closed %s\n", file);
 
 	free(sb);
 
